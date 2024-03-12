173e3b1006894ed5167128d5d0c4d3b36325c8c5
AxelADMIN <axelghigo@gmail.com>
2024-03-12 20:14:32 +0100

    miglioramento performans

--- src/utils/lib/callAPI/payment.js
+++ src/utils/lib/callAPI/payment.js
@@ -8,16 +8,16 @@ export const callPaymentAPI = {
       },
     }),
 
-  create: async (obj) =>
-    fetch('https://api-parent-pay.vercel.app/api/v1/posts', {
+  create: async (obj, query = '') =>
+    fetch(`https://api-parent-pay.vercel.app/api/v1/posts${query}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(obj),
     }),
-  update: async (id, obj) =>
-    fetch(`https://api-parent-pay.vercel.app/api/v1/posts/${id}`, {
+  update: async (id, obj, query = '') =>
+    fetch(`https://api-parent-pay.vercel.app/api/v1/posts/${id}${query}`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
@@ -25,7 +25,7 @@ export const callPaymentAPI = {
       body: JSON.stringify(obj),
     }),
 
-  delete: async (id, query) =>
+  delete: async (id, query = '') =>
     fetch(`https://api-parent-pay.vercel.app/api/v1/posts/${id}${query}`, {
       method: 'DELETE',
     }),
